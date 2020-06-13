ca261b02a420edd4850b461c07700fe7ed7150a2
TahaMagdy <t.tahamagdyt@outlook.com>
2020-06-13 18:58:22 +0200

    random_bot: now can take a board and play a turn

--- BuildingBlocks/RandomBot/RandomBot.py
+++ BuildingBlocks/RandomBot/RandomBot.py
@@ -1,31 +1,24 @@
 import random
 
-"""
-0  -> x
-1  -> o
-2 -> empty
-"""
+X = 0
+O = 1
+NULL = 2
 
-XOboard = [[2,2,2],
-           [2,2,2],
-           [2,2,2]]
+XOboard = [[NULL,NULL,NULL],
+           [NULL,NULL,NULL],
+           [NULL,NULL,NULL]]
 
 def get_empty_cells(board):
     empty_cells = set()
     for i, row in enumerate(board):
         for j, _ in enumerate(row):
-            if board[i][j] == 2:
+            if board[i][j] == NULL:
                 empty_cells.add( (i, j) )
     return empty_cells
 
 
 def random_bot(board, role):
-    """
-        role: belongs to {1, 0, 2}
-    """
     emptyCells = get_empty_cells(board)
-    x, y = random.randint(0, len(emptyCells)-1)
-    board[x][y] = role
+    i, j = random.sample(emptyCells, 1)[0]
+    board[i][j] = role
     return board
-
-print(get_empty_cells(XOboard))
