316d4cbfd9acc5b58bb7f1acc66f5d16655af797
ahmedamrelmahdy <elmahdy.ahmed@hotmail.com>
2020-06-14 22:32:09 +0200

    add my implementation to random player

--- BuildingBlocks/Engine/Player.py
+++ BuildingBlocks/Engine/Player.py
@@ -1,10 +1,19 @@
 from Engine import possibilities
+import random
+
+
+def sequential_player(board, player):
+    # Play turn in sequential order
+    selection = possibilities(board)
+    current_loc = selection[0]
+    board[current_loc] = player
+    return board
 
 
 def random_player(board, player):
     # Play turn in sequential order
     selection = possibilities(board)
-    current_loc = selection[0]
+    current_loc = random.choice(selection)
     board[current_loc] = player
     return board
 
@@ -19,5 +28,9 @@ def human_player(board, player):
 
 
 def player_dict(board, player, player_type):
-    dict_players = {'h': human_player, 'r': random_player}
+    dict_players = {
+            'h': human_player,
+            's': sequential_player,
+            'r': random_player
+            }
     return dict_players[player_type](board, player)
