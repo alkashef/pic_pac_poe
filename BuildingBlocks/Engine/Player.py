09c0317f42b5e691ae9b7f4dcc05f85dacd16692
ahmedamrelmahdy <elmahdy.ahmed@hotmail.com>
2020-06-17 04:03:09 +0200

    Transform project to oop

--- BuildingBlocks/Engine/Player.py
+++ BuildingBlocks/Engine/Player.py
@@ -1,36 +1,15 @@
-from Engine import possibilities
 import random
 
 
-def sequential_player(board, player):
-    # Play turn in sequential order
-    selection = possibilities(board)
-    current_loc = selection[0]
-    board[current_loc] = player
-    return board
+class Player:
+    def __init__(self, mark, play_tactic):
+        self.mark = mark
+        self.play_tactic = play_tactic
 
 
-def random_player(board, player):
-    # Play turn in sequential order
-    selection = possibilities(board)
-    current_loc = random.choice(selection)
-    board[current_loc] = player
-    return board
+    def random_player(board):
+        # select random empty position in board
+        empty_slots_tuple = board.empty_slots()
+        current_loc = random.choice(empty_slots_tuple)
+        return current_loc
 
-
-def human_player(board, player):
-    # Play turn using input
-    selection = possibilities(board)
-    print(f'Your move :{selection}')
-    current_loc = eval(input())
-    board[current_loc] = player
-    return board
-
-
-def player_dict(board, player, player_type):
-    dict_players = {
-            'h': human_player,
-            's': sequential_player,
-            'r': random_player
-            }
-    return dict_players[player_type](board, player)
