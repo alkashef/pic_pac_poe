b7a54234b5ae85590cd7713c876f055c779f46e8
TahaMagdy <t.tahamagdyt@outlook.com>
2020-06-21 18:11:18 +0200

    add heristic player

--- BuildingBlocks/Bots/heuristicPlayer.py
+++ BuildingBlocks/Bots/heuristicPlayer.py
@@ -3,7 +3,24 @@ import Board
 import Role
 
 
-class heuristicPlayer:
+class HeuristicPlayer:
 
     def heuristic_play(self, board, role):
-        pass
+        """
+            1 Do Not Lose
+            2 Win
+        """
+        print("Heuristic Play")
+
+
+if __name__ == "__main__":
+    board = Board.Board()
+    player = HeuristicPlayer()
+    
+    board.board[0][0] = 1
+    #board.board[0][2] = 1
+    board.board[1][1] = 1
+    board.board[2][2] = 1
+    board.board[2][0] = 1
+    board.show()
+    board.get_winning_cells_for(Role.O)
