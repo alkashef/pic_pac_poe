0fcee260bb94cf04a0d3cca98dc0461dfa37b093
TahaMagdy <t.tahamagdyt@outlook.com>
2020-06-21 16:53:57 +0200

    adding show

--- BuildingBlocks/Bots/Board.py
+++ BuildingBlocks/Bots/Board.py
@@ -1,19 +1,26 @@
 # I think there is a better way to handel those states
-X = 0
-O = 1
-NULL = 2
+import Role
+
+
 
 class Board:
-    board = [[NULL,NULL,NULL],
-             [NULL,NULL,NULL],
-             [NULL,NULL,NULL]]
+    board = [[Role.NULL,Role.NULL,Role.NULL],
+             [Role.NULL,Role.NULL,Role.NULL],
+             [Role.NULL,Role.NULL,Role.NULL]]
 
     def get_empty_cells(self):
         empty_cells = set()
         for i, row in enumerate(self.board):
             for j, _ in enumerate(row):
-                if self.board[i][j] == NULL:
+                if self.board[i][j] == Role.NULL:
                     empty_cells.add( (i, j) )
         return empty_cells
+
+    def set_cell(self, i, j, role):
+        self.board[i][j] = role
+
+    def show(self):
+        for i in self.board:
+            print(i)
 
 
