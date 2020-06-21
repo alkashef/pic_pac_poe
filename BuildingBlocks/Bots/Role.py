1d7311b0aa1ad2c787e6773b0008591f3f33269c
TahaMagdy <t.tahamagdyt@outlook.com>
2020-06-21 17:08:13 +0200

    add get_opponent()

--- BuildingBlocks/Bots/Role.py
+++ BuildingBlocks/Bots/Role.py
@@ -1,3 +1,11 @@
 X = 0
 O = 1
 NULL = 2
+
+def get_opponent(given):
+    if given == X:
+        return O
+    if given == O:
+        return X
+    else:
+        raise "Given Must be Role.X or Role.O"
